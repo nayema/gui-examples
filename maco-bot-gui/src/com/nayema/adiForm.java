57e4b25b3b5ddafda5804617411c1dafde8534d0
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-13 17:26:45 -0400

    Creates two gui forms for calculating MACO and ADI

---
+++ maco-bot-gui/src/com/nayema/adiForm.java
@@ -0,0 +1,21 @@
+package com.nayema;
+
+import javax.swing.*;
+
+public class adiForm {
+    private JPanel mainPanel;
+    private JButton calculateADIButton;
+    private JTextField calculatedADITextField;
+    private JTextField clientNameTextField;
+    private JTextField activeNameTextField;
+    private JTextField textField4;
+    private JTextField minDailyDoseTextField;
+    private JLabel activeNameLabel;
+    private JLabel clientNameLabel;
+    private JLabel unitLabel;
+    private JLabel unitLabel2;
+    private JLabel ttcLabel;
+    private JLabel mddLabel;
+    private JComboBox ttcComboBox;
+    private JPanel calculateADIPanel;
+}
---
+++ maco-bot-gui/src/com/nayema/macoForm.java
@@ -0,0 +1,23 @@
+package com.nayema;
+
+import javax.swing.*;
+
+public class macoForm {
+
+    private JPanel mainPanel;
+    private JLabel clientNameLabel;
+    private JLabel productActiveNameLabel;
+    private JLabel cleaningMethodLabel;
+    private JLabel adiLabel;
+    private JLabel equipmentListLabel;
+    private JTable table1;
+    private JButton enterEquipmentTrainButton;
+    private JLabel productCodeLabel;
+    private JButton calculateL4Button;
+    private JTextArea productCodeSTextArea;
+    private JTextField activeNameTextField;
+    private JTextField clientNameTextField;
+    private JTextField cleaningMethodTextField;
+    private JComboBox adiComboBox;
+    private JTextField macoResultTextField;
+}
