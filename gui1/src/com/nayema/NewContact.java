e2782298528ce8a07b8c49c002dcfe8e01bfc0c7
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-13 17:27:37 -0400

    Creates New Contact form using multiple JPanels

---
+++ gui1/src/com/nayema/NewContact.java
@@ -0,0 +1,20 @@
+package com.nayema;
+
+import javax.swing.*;
+
+public class NewContact {
+    private JPanel newContactRootPanel;
+    private JPanel westLabelPanel;
+    private JPanel eastTextFieldPanel;
+    private JPanel southButtons;
+    private JLabel givenNameLabel;
+    private JLabel surnameLabel;
+    private JLabel mobileLabel;
+    private JLabel emailLabel;
+    private JTextField givenNamesTextField;
+    private JTextField surnameTextField;
+    private JTextField mobileTextField;
+    private JTextField emailTextField;
+    private JButton addContactButton;
+    private JButton noOp;
+}
