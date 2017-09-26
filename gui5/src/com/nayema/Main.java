31d064139677f8bdc3ddbb7b8e6cf699598c44d7
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-26 16:02:23 -0400

    Creates a JTable based on stored data in a 2D array

--- gui5/src/com/nayema/Main.java
+++ gui5/src/com/nayema/Main.java
@@ -28,20 +28,33 @@ public class Main {
     }
 
     class TableData extends AbstractTableModel {
+        String[][] allData = new String[3][3];
+
+        public TableData() {
+            allData[0][0] = "1";
+            allData[0][1] = "2";
+            allData[0][2] = "3";
+            allData[1][0] = "4";
+            allData[1][1] = "5";
+            allData[1][2] = "6";
+            allData[2][0] = "7";
+            allData[2][1] = "8";
+            allData[2][2] = "9";
+        }
 
         @Override
         public int getRowCount() {
-            return 10;
+            return allData.length;
         }
 
         @Override
         public int getColumnCount() {
-            return 10;
+            return allData[0].length;
         }
 
         @Override
         public Object getValueAt(int rowIndex, int columnIndex) {
-            return "Hello";
+            return allData[rowIndex][columnIndex];
         }
     }
 }
